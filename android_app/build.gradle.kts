61bb945d22df96e386e79a91ea8615b83d5a2b25
chenshuang <1600662882@qq.com>
2026-06-16 14:43:01 +0800

    fix: add JAXB dependency and JVM args for Java 17 compatibility

--- android_app/build.gradle.kts
+++ android_app/build.gradle.kts
@@ -1,3 +1,9 @@
+buildscript {
+    dependencies {
+        classpath("jakarta.xml.bind:jakarta.xml.bind-api:4.0.0")
+    }
+}
+
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
